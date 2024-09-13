cc21ac86afadde86900d96eebed4a40d5bb77d4f
lxishh <henrix913@gmail.com>
2024-09-13 11:26:48 -0300

    views/url/html

--- appAnimales/views.py
+++ appAnimales/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def renderListado(request):
+    return render(request, 'appTemplate/listado.html')
--- proyectoAnimales/urls.py
+++ proyectoAnimales/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appAnimales.views import renderListado
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('listado/', renderListado)
 ]
